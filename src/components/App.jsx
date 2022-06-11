30f2c617657e53e176e60c2f996061210bfcf0be
Yuliia Shybyk <j7557500@gmail.com>
2022-06-12 00:07:33 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
+import user from "components/json/user.json"
+// import data from "components/data/data.json"
+// import friends from "components/friends/friends.json"
+// import transactions from "components/transactions/transactions.json"
+
+import Profile from "components/user/User";
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#7755e6'
-      }}
-    >
-      Deploy test
+return (
+    <div>
+      <Profile
+        avatar={user.avatar}
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
     </div>
-  );
+  )
 };
---
+++ src/components/user/User.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import s from 'components/user/User.module.css';
+
+export default function Profile({
+  avatar,
+  username,
+  tag,
+  location,
+  followers,
+  views,
+  likes,
+}) {
+  return (
+    <div className={s.profile}>
+      <div className={s.description}>
+        <img src={avatar} alt={username} className={s.avatar} />
+        <p className={s.name}>{username}</p>
+        <p className={s.tag}>@{tag}</p>
+        <p className={s.location}>{location}</p>
+      </div>
+
+      <ul className={s.stats}>
+        <li>
+          <span className={s.label}>Followers </span>
+          <span className={s.quantity}>{followers}</span>
+        </li>
+        <li>
+          <span className={s.label}>Views </span>
+          <span className={s.quantity}>{views}</span>
+        </li>
+        <li>
+          <span className={s.label}>Likes </span>
+          <span className={s.quantity}>{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  followers: PropTypes.number.isRequired,
+  views: PropTypes.number.isRequired,
+  likes: PropTypes.number.isRequired,
+};
